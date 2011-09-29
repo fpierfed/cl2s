329865c79402a5dee4e85025a6b612b0d11dd5e1
Francesco Pierfederici <fpierfed@me.com>
2011-09-29 17:48:31 -0400

    moved to ClassAd support for more generality.

--- src/Job.py
+++ src/Job.py
@@ -1,18 +1,47 @@
-from CL2SObject import CL2SObject
+import uuid
+
+from ClassAd import ClassAd
 
 
 
 
 
-class Job(CL2SObject):
+class Job(ClassAd):
     """
-    This is a process scheduled to run (or runnning) on the cluster.
+    A Job is a specialized ClassAd that has information on how to execute a 
+    given command on some given data on a compute node.
+    
+    The main differences with respect to standard Condor Job ClassAds are:
+    1. Job instances have an ID associated to them to uniquely identify them in 
+       the job queue and while executing.
+    2. Job instances always have a JobState/jobstate attribute. When the 
+       original ClassAd did not have one, the corresponding Job instance will 
+       have it set to 'Queued'.
+    3. (Done in ClasAd.py) Queue is transformed into CL2S_INSTANCES.
+    4. Job instances have a CL2S_DATASET variable corresponding to the ClassAd
+       InputDataset attribute.
+    
     """
-    def __init__(self, class_ad, dataset):
-        self.class_ad = class_ad
-        self.dataset = dataset
+    def __init__(self, ad):
+        """
+        Create a Job instance from the input text ClassAd `ad`. This also 
+        creates the following instance variables:
+            CL2S_JOB_ID = uuid.uuid4()
+            JobState = 'Queued' if Job.JobState is not defined
+            CL2S_INSTANCES = max(1, N) from the Queue directive
+            CL2S_DATASET = ad.InputDataset
+        """
+        # Invoke the superclass constructor.
+        super(self, Job).__init__(ad)
+        
+        # Create our extra instance variables. Modify self._ad_attributes to 
+        # reflect the new variables we have created.
+        self.CL2S_JOB_ID = unicode(uuid.uuid4())
+        self.CL2S_DATASET = getattr(self, 'InputDataset', None)
+        if(not hasattr(self, 'JobState')):
+            self.JobState = 'Queued'
+            self._ad_attributes.append('JobState')
+        self._ad_attributes += ['CL2S_JOB_ID', 'CL2S_DATASET']
         return
-    
-    
-    def __repr__(self):
-        return(self.class_ad)
+
+
